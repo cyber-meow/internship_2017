e93994e47c517ca602c8203ece799a6c9a1412b4
cyber-meow <sjungle305@gmail.com>
2017-06-30 20:26:58 +0200

    routine ok

---
+++ classify.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import tensorflow as tf
+from datasets import dataset_utils
+from nets import inception_v4
+from preprocessing import inception_preprocessing
+
+
+slim = tf.contrib.slim
+
+
+def classify_image(image_path, train_dir, label_dir):
+
+    image_size = 299
+
+    with tf.Graph().as_default():
+
+        image_string = tf.gfile.FastGFile(image_path, 'r').read()
+        _, image_ext = os.path.splitext(image_path)
+
+        if image_ext == '.jpg':
+            image = tf.image.decode_jpg(image_string, channels=3)
+        if image_ext == '.png':
+            image = tf.image.decode_png(image_string, channels=3)
+        else:
+            raise ValueError('image format not supported, must be jpg or png')
+
+        processed_image = inception_preprocessing.preprocess_image(
+            image, image_size, image_size, is_training=False)
+        processed_images = tf.expand_dims(processed_image, 0)
+
+        labels_to_names = dataset_utils.read_label_file(label_dir)
+
+        # Create the model, use the default arg scope to
+        # configure the batch norm parameters.
+        with slim.arg_scope(inception_v4.inception_v4_arg_scope()):
+            logits, endpoints = inception_v4.inception_v4(
+                processed_images, num_classes=len(labels_to_names),
+                is_training=False)
+        probabilities = endpoints['Predictions']
+
+        checkpoint_path = tf.train.latest_checkpoint(train_dir)
+        saver = tf.train.Saver(tf.model_variables())
+
+        with tf.Session() as sess:
+            saver.restore(sess, checkpoint_path)
+
+            with slim.queues.QueueRunners(sess):
+                probabilities = sess.run(probabilities)
+
+            probabilities = probabilities[0, 0:]
+            sorted_inds = [i[0] for i in sorted(
+                enumerate(-probabilities), key=lambda x:x[1])]
+
+        for i in range(5):
+            index = sorted_inds[i]
+            print('Probability %0.2f%% => [%s]' % (
+                  probabilities[index] * 100, labels_to_names[index]))
---
+++ evaluate.py
@@ -0,0 +1,119 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from six.moves import xrange
+
+import time
+
+import tensorflow as tf
+from nets import inception_v4
+
+import data.read_TFRecord as read_TFRecord
+from data.load_batch import load_batch
+
+
+slim = tf.contrib.slim
+
+
+def eval_step(sess, fw, accuracy, accuracy_summary, global_step_op, *args):
+
+    tensors_to_run = [accuracy, accuracy_summary, global_step_op]
+    tensors_to_run.extend(args)
+
+    start_time = time.time()
+    tensor_values = sess.run(tensors_to_run)
+    time_elapsed = time.time() - start_time
+
+    accuracy_rate = tensor_values[0]
+    accuracy_summary_serialized = tensor_values[1]
+    global_step_count = tensor_values[2]
+
+    fw.add_summary(accuracy_summary_serialized, global_step=global_step_count)
+
+    tf.logging.info(
+        'global step %s: accurarcy: %.4f (%.2f sec/step)',
+        global_step_count, accuracy_rate, time_elapsed)
+
+    return tensor_values
+
+
+def evaluate(dataset_dir,
+             train_dir,
+             log_dir,
+             number_of_steps=None,
+             batch_size=12):
+
+    if not tf.gfile.Exists(log_dir):
+        tf.gfile.MakeDirs(log_dir)
+
+    image_size = 299
+
+    with tf.Graph().as_default():
+
+        tf.logging.set_verbosity(tf.logging.INFO)
+
+        dataset = read_TFRecord.get_split('validation', dataset_dir)
+        images, _, labels = load_batch(
+            dataset, height=image_size, width=image_size,
+            batch_size=batch_size, is_training=False)
+
+        if number_of_steps is None:
+            number_of_steps = dataset.number_samples / batch_size
+
+        with slim.arg_scope(inception_v4.inception_v4_arg_scope()):
+            logits, endpoints = inception_v4.inception_v4(
+                images, num_classes=dataset.num_classes, is_training=False)
+
+        # Define metric
+        predictions = tf.argmax(endpoints['Predictions'], 1)
+        accuracy = tf.reduce_mean(tf.cast(
+            tf.equal(predictions, labels), tf.float32))
+        accuracy_summary = tf.summary.scalar('accuracy', accuracy)
+
+        # Define global step to be show in tensorboard
+        global_step = tf.train.get_or_create_global_step()
+        global_step_op = tf.assign(global_step, global_step+1)
+
+        # File writer for the tensorboard
+        fw = tf.summary.FileWriter(log_dir)
+
+        checkpoint_path = tf.train.latest_checkpoint(train_dir)
+        saver = tf.train.Saver(tf.model_variables())
+
+        with tf.Session() as sess:
+            with slim.queues.QueueRunners(sess):
+                sess.run(tf.variables_initializer([global_step]))
+                sess.run(tf.local_variables_initializer())
+                saver.restore(sess, checkpoint_path)
+
+                for step in xrange(number_of_steps-1):
+                    eval_step(sess, fw, accuracy,
+                              accuracy_summary, global_step_op)
+
+                global_step_count, labels, predictions, images = eval_step(
+                    sess, fw, accuracy, accuracy_summary, global_step_op,
+                    labels, predictions, images)[2:]
+                true_names = [
+                    dataset.labels_to_names[i] for i in labels]
+                predicted_names = [
+                    dataset.labels_to_names[i] for i in predictions]
+
+                tf.logging.info('Information for the last batch')
+                tf.logging.info('Ground Truth: [%s]', true_names)
+                tf.logging.info('Prediciotn: [%s]', predicted_names)
+
+                with tf.name_scope('last_images'):
+                    for i in range(batch_size):
+                        image_pl = tf.placeholder(
+                            dtype=tf.float32,
+                            shape=(1, image_size, image_size, 3))
+                        image_summary = tf.summary.image(
+                            'image_true_{}_predicted_{}'.format(
+                                true_names[i], predicted_names[i]), image_pl)
+                        fw.add_summary(
+                            sess.run(image_summary,
+                                     feed_dict={image_pl: [images[i]]}),
+                            global_step=global_step_count)
+
+                tf.logging.info('Finished evaluation.')
